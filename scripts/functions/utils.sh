ddad2085934310333bc8ce748826b564ef7a44b1
ocelus <contact@ftnl.fr>
2026-01-20 22:18:25 +0100

    feat: enhance logging format with colored output and timestamps
    Updated the logging function to include ANSI color codes for different log levels and added a timestamp to improve readability and debugging.

--- scripts/functions/utils.sh
+++ scripts/functions/utils.sh
@@ -4,7 +4,31 @@ send_log(){
   local type="$1"
   local message="$2"
   local log_level="${3:-INFO}"
-  echo "[HytaleDockerServer $type]: $message" >&2
+
+  # Définition des couleurs ANSI
+  local C_RESET='\033[0m'
+  local C_YELLOW='\033[0;33m'
+  local C_CYAN='\033[0;36m'
+  local C_RED='\033[0;31m'
+
+  local level_color=""
+  case "$log_level" in
+      "INFO")  level_color="$C_CYAN" ;;
+      "WARN")  level_color="$C_YELLOW" ;;
+      "ERROR") level_color="$C_RED" ;;
+      *)       level_color="$C_RESET" ;;
+  esac
+
+  local timestamp=""
+  timestamp=$(date "+%Y/%m/%d %H:%M:%S")
+  printf -v formated \
+    "[HytaleDockerServer] ${C_YELLOW}[%-19s${level_color}%10s${C_YELLOW}]%15s${C_RESET} ${level_color}%s${C_RESET}" \
+      "$timestamp" \
+      "$log_level" \
+      "[$type]" \
+      "$message"
+
+  echo -e "$formated" >&2
 }
 
 get_json_val() {
